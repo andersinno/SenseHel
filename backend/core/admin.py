08e16be5a5c3e1b282f7d58c52ae3bf6e0415e40
Marko Koivusalo <marko.koivusalo@gofore.com>
2018-12-12 14:46:20 +0200

    Added models to django admin site

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Apartment, Sensor, Service, ServiceRequirement, SensorProvides
+
+admin.site.register(Apartment)
+admin.site.register(Sensor)
+admin.site.register(Service)
+admin.site.register(ServiceRequirement)
+admin.site.register(SensorProvides)
