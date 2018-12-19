23f9cc6a69a90ae8582a51163e1753943ff9286f
MariaSyed <mariasy@metropolia.fi>
2018-12-19 15:50:07 +0200

    Control inputs with state

--- frontend/src/components/LoginButton/index.js
+++ frontend/src/components/LoginButton/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './loginbutton.styles.css';
 
-const LoginButton = () => (
-  <button className="button" type="submit">
+const LoginButton = ({ ...props }) => (
+  <button className="button" type="submit" {...props}>
     <p className="large-body">SIGN IN</p>
   </button>
 );
--- frontend/src/containers/Login/index.js
+++ frontend/src/containers/Login/index.js
@@ -6,18 +6,39 @@ import TextInput from '../../components/TextInput';
 import LoginButton from '../../components/LoginButton';
 
 class Index extends Component {
+  state = {
+    userNumber: '',
+    pinCode: ''
+  };
+
+  onChangeInput = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   onLogin = () => {};
 
   render() {
+    const { userNumber, pinCode } = this.state;
+
     return (
       <div className="login-page">
         <div className="login-page__content">
           <img className="content__img-logo" src={Images.Logo} alt="logo" />
 
           <div className="content__input-container">
-            <TextInput placeholder="User number" />
+            <TextInput
+              placeholder="User number"
+              name="userNumber"
+              value={userNumber}
+              onChange={this.onChangeInput}
+            />
 
-            <TextInput placeholder="PIN code" />
+            <TextInput
+              placeholder="PIN code"
+              name="pinCode"
+              value={pinCode}
+              onChange={this.onChangeInput}
+            />
           </div>
 
           <div className="content__button-container">
