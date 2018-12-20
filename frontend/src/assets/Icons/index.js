fb30ff885ea67b822403233f05ce4ffa2753ddda
MariaSyed <mariasy@metropolia.fi>
2018-12-20 15:12:38 +0200

    Add card header with icons

--- frontend/src/assets/Icons/index.js
+++ frontend/src/assets/Icons/index.js
@@ -2,8 +2,20 @@
 
 export default {
   CO2: require('./Carbon dioxide.png'),
+  CO2_Normal: require('./Carbon Dioxide_Green.png'),
+  CO2_Warning: require('./Carbon Dioxide_Orange.png'),
+  CO2_Danger: require('./Carbon Dioxide_Red.png'),
+
   Humidity: require('./Humidity.png'),
+  Humidity_Normal: require('./Humidity_Green.png'),
+  Humidity_Warning: require('./Humidity_Orange.png'),
+  Humidity_Danger: require('./Humidity_Red.png'),
+
   Temperature: require('./Temperature.png'),
+  Temperature_Normal: require('./Temperature_Green.png'),
+  Temperature_Warning: require('./Temperature_Orange.png'),
+  Temperature_Danger: require('./Temperature_Red.png'),
+
   Success: require('./Success.png'),
   Failure: require('./Fail.png'),
   About_Icon: require('./About.png'),
--- frontend/src/components/SubscribedServiceCard/index.js
+++ frontend/src/components/SubscribedServiceCard/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import './subscribedservicecard.styles.css';
+import Icons from '../../assets/Icons';
 
 const SubscribedServiceCard = () => (
-  <div className="service-card">Temperature</div>
+  <div className="service-card">
+    <div className="service-card__header">
+      <span className="body-text">Temperature</span>
+      <img
+        className="service-card__header__icon"
+        src={Icons.Temperature_Warning}
+        alt="temperature"
+      />
+    </div>
+  </div>
 );
 
 export default SubscribedServiceCard;
