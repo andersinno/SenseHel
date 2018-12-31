24814533840d3558f4aa8d2b776bffd8af5e779b
MariaSyed <mariasy@metropolia.fi>
2018-12-31 11:14:51 +0200

    Add expand state and replace button with material ui button

--- frontend/src/components/OfferedServiceCard/index.js
+++ frontend/src/components/OfferedServiceCard/index.js
@@ -1,50 +1,80 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import IconButton from '@material-ui/core/IconButton';
+
 import './offeredservicecard.styles.css';
 import Icons from '../../assets/Icons';
 
-const OfferedServiceCard = ({ image, name, description, price, benefit }) => (
-  <div className="offered-service-card">
-    <div className="offered-service-card__row1">
-      <div className="offered-service-card__col1">
-        <img src={image} className="col1__img" alt="service" />
-      </div>
+class OfferedServiceCard extends Component {
+  state = {
+    expanded: false
+  };
 
-      <div className="offered-service-card__col2">
-        <p className="headline offered-service-card__text">{name}</p>
-        <p className="body-text offered-service-card__text">{description}</p>
-      </div>
+  handleExpandClick = () => {
+    const { expanded } = this.state;
+    this.setState({ expanded: !expanded });
+  };
 
-      <div className="offered-service-card__col3">
-        <img src={Icons.Arrow_Up} className="col3__icon" alt="arrow-down" />
-      </div>
-    </div>
+  render() {
+    const { image, name, description, price, benefit } = this.props;
+    const { expanded } = this.state;
+    const showMoreIconExpandedClass =
+      expanded && 'col3__show-more-icon-clicked';
 
-    <div className="offered-service-card__row2">
-      <div className="offered-service-card__col1" />
-      <div className="offered-service-card__col2">
-        <table className="row2__col2__table">
-          <tr>
-            <th className="body-text">Price</th>
-            <th className="body-text">Benefit</th>
-          </tr>
+    return (
+      <div className="offered-service-card">
+        <div className="offered-service-card__row1">
+          <div className="offered-service-card__col1">
+            <img src={image} className="col1__img" alt="service" />
+          </div>
 
-          <tr>
-            <td className="body-text">
-              <b>{price}</b>
-            </td>
-            <td className="body-text">
-              <img
-                className="benefit-col__body-text__icon"
-                src={Icons.Green_Arrow}
-                alt="green arrow"
-              />
-              <b>{benefit}%</b>
-            </td>
-          </tr>
-        </table>
+          <div className="offered-service-card__col2">
+            <p className="headline offered-service-card__text">{name}</p>
+            <p className="body-text offered-service-card__text">
+              {description}
+            </p>
+          </div>
+
+          <div className="offered-service-card__col3">
+            <IconButton
+              className={`col3__show-more-icon ${showMoreIconExpandedClass}`}
+              onClick={this.handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="Show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </div>
+        </div>
+
+        <div className="offered-service-card__row2">
+          <div className="offered-service-card__col1" />
+          <div className="offered-service-card__col2">
+            <table className="row2__col2__table">
+              <tr>
+                <th className="body-text">Price</th>
+                <th className="body-text">Benefit</th>
+              </tr>
+
+              <tr>
+                <td className="body-text">
+                  <b>{price}</b>
+                </td>
+                <td className="body-text">
+                  <img
+                    className="benefit-col__body-text__icon"
+                    src={Icons.Green_Arrow}
+                    alt="green arrow"
+                  />
+                  <b>{benefit}%</b>
+                </td>
+              </tr>
+            </table>
+          </div>
+        </div>
       </div>
-    </div>
-  </div>
-);
+    );
+  }
+}
 
 export default OfferedServiceCard;
