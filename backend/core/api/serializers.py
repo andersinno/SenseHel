1abc52a7c75de90293e0a290ec77eb069a4e12b7
Marko Koivusalo <marko.koivusalo@gofore.com>
2019-01-04 15:29:01 +0200

    Added apartment-services and subscribed-services backend API

--- backend/core/api/serializers.py
+++ backend/core/api/serializers.py
@@ -1,15 +1,21 @@
 from rest_framework import serializers
 
-from ..models import Service, Apartment
+from ..models import Service, Apartment, Subscription
 
 
 class ServiceSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Service
-        fields = ('name', 'description', 'price', 'benefit_short', 'benefit_long', 'eula_url')
+        fields = ('id', 'name', 'description', 'price', 'benefit_short', 'benefit_long', 'eula_url')
 
 
 class ApartmentSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Apartment
         fields = ('first_name', 'last_name', 'street', 'city', 'postal_code', 'phone')
+
+
+class SubscriptionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ('id', 'created_at', 'updated_at', 'service', 'apartment')
